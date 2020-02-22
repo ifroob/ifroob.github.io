71699c69fcdaf1da9ede00fd7d61fc43f013f3b5
Brian Ta <9froob9@gmail.com>
2020-02-22 07:45:27 -0600

    Updates

---
+++ precache-manifest.e9299ee1d004568da14a406e544f8ef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1695d8687890580fab5f600e1fe23f23",
+    "url": "/ifroob-portfolio/index.html"
+  },
+  {
+    "revision": "12d2a9c4e753324a1678",
+    "url": "/ifroob-portfolio/static/css/main.af4139ed.chunk.css"
+  },
+  {
+    "revision": "0ecbdc0d14dccedb0930",
+    "url": "/ifroob-portfolio/static/js/2.e3ddbf3a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ifroob-portfolio/static/js/2.e3ddbf3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d2a9c4e753324a1678",
+    "url": "/ifroob-portfolio/static/js/main.759fba57.chunk.js"
+  },
+  {
+    "revision": "d2b31ad51a0d5a13449d",
+    "url": "/ifroob-portfolio/static/js/runtime-main.11bb0a25.js"
+  },
+  {
+    "revision": "e06092d5d1be50d3f7787435d5346492",
+    "url": "/ifroob-portfolio/static/media/banner4.e06092d5.jpg"
+  }
+]);
